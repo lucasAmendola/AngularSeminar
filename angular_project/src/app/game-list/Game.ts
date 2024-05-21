4694603e758410f2dd545da0a677521405cd63fb
Lucas Amendola <lukitasromanpiles7@gmail.com>
2024-05-21 18:57:56 -0300

    refactorizacion de estilos y distribucion de la web

--- angular_project/src/app/game-list/Game.ts
+++ angular_project/src/app/game-list/Game.ts
@@ -1,9 +1,12 @@
 export interface Game {
-    image : string
+    image : string;
     name : string;
+    description: string;
     genre : string;
     price : number;
     stock : number;
     offer : boolean;
     quantity: number;
+    jugability: string;
+    jugability2:string;
 }
